1f2f9dab4d35d77309928973252fa947fa731e40
naong <naong4ha@gmail.com>
2025-03-09 11:47:53 +0900

    상위div가 position:relative가 안되어있어서 div의 기준위치가 다른곳으로 잡혀있었음.

--- src/pages/Math2.jsx
+++ src/pages/Math2.jsx
@@ -5,8 +5,8 @@ import ImageContainer from "../components/ImageContainer"
 export default function Math2() {
   const divSettings = [
     { index: 2, divs: [
-      { className: 'div1', width: '112px', height: '39px', top: '216px', left: '196px' },
-      { className: 'div2', width: '102px', height: '12px', top: '297px', left: '200px' },
+      { className: 'div1', width: '112px', height: '39px', top: '106px', left: '15px' },
+      { className: 'div2', width: '102px', height: '12px', top: '186px', left: '19px' },
     ]},
     { index: 3, divs: [
       { className: 'div1', width: '250px', height: '200px', top: '60px', left: '100px' },
